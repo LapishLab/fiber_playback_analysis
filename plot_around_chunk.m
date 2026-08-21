d1570155b8fa78f589e7a94d85cb481e79fab52a
David Swygart <davidswygart@gmail.com>
2026-08-21 15:16:35 -0400

    add argument for plotting specific chunk ind

--- plot_around_chunk.m
+++ plot_around_chunk.m
@@ -1,32 +1,43 @@
-function plot_around_chunk(play_info, gc, tm)
+function plot_around_chunk(play_info, gc, tm, opt)
+    arguments
+        play_info
+        gc
+        tm
+        opt.chunk_inds = 1
+    end
     %% Get Chunk ON times
     inds = find_chunk_inds(play_info.Type, 'USV');
     chunk_time_usv = play_info.posix(inds);
-    
-    inds = find_chunk_inds(play_info.Type, 'background');
-    chunk_time_back = play_info.posix(inds);
-    
-    %% Histogram of chunk durations
-    figure(1); clf
-    subplot(2,1,1)
-    histogram(chunk_time_back-chunk_time_usv)
-    title('USV chunk')
-    xlabel('Duration (s)')
-    ylabel('Count')
-    
-    subplot(2,1,2)
-    histogram(chunk_time_usv(2:end)-chunk_time_back(1:end-1))
-    title('Background chunk')
-    xlabel('Duration (s)')
-    ylabel('Count')
+        
+    % %% Histogram of chunk durations
+    % inds = find_chunk_inds(play_info.Type, 'background');
+    % chunk_time_back = play_info.posix(inds);
+
+    % figure(1); clf
+    % subplot(2,1,1)
+    % histogram(chunk_time_back-chunk_time_usv)
+    % title('USV chunk')
+    % xlabel('Duration (s)')
+    % ylabel('Count')
+    % 
+    % subplot(2,1,2)
+    % histogram(chunk_time_usv(2:end)-chunk_time_back(1:end-1))
+    % title('Background chunk')
+    % xlabel('Duration (s)')
+    % ylabel('Count')
     %% Plot chunks of ON/OFF
     pre_time = 20;
     post_time = 20;
-    t = chunk_time_usv(2:4); % skip the first USV chunk as it doesn't have preceeding background
+    t = chunk_time_usv(opt.chunk_inds); % Which chunks to plot. Maybe not first since it doesn't have a preceeding background
+
     [fiber_usv, fiber_tm] = get_around_inds(gc, tm, t, pre_time, post_time);
     %%
     figure(1); clf; hold on
-    shadedErrorBar(fiber_tm, fiber_usv, {@mean, @nan_sem}, 'lineprops', {'Color', 'b'})
+    if length(opt.chunk_inds)>1
+        shadedErrorBar(fiber_tm, fiber_usv, {@mean, @nan_sem}, 'lineprops', {'Color', 'b'})
+    else
+        plot(fiber_tm, fiber_usv, 'Color', 'b')
+    end
     xline(0,'--k')
     xlabel('Time after USV chunk start (s)')
     %%
