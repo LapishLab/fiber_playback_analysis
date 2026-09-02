cb8139e0be693d2a9000e6216a24d7a2c40ccd79
David Swygart <davidswygart@gmail.com>
2026-09-02 17:30:44 -0400

    Limit to max time for start estimate

--- estimate_play_start.m
+++ estimate_play_start.m
@@ -1,6 +1,8 @@
 function start = estimate_play_start(spect, sr)
     %restrict to first 10 minutes of audio
-    spect = spect(:, 1:sr*10*60);
+    max_t = sr*10*60;
+    max_t = min(max_t, width(spect));
+    spect = spect(:, 1:max_t);
     
     windowLength=30*sr;
     y = movmedian(mean(spect), windowLength);
